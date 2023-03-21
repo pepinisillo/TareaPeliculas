0adbefe0ef6bec349ae1c8b26bb1d8dfa8dbd294
Alejandro <126518765+pepinisillo@users.noreply.github.com>
2023-03-21 08:31:13 +0000

    Segunda Versión(2)

--- Pelicula/Program.cs
+++ Pelicula/Program.cs
@@ -8,28 +8,34 @@ namespace Pelicula
     {
         //Campos
         private List<Actor> actores = new List<Actor>();
-        public string titulo;
-        public Int16 año;
-        public String pais;
-        public string director;
+        private string titulo;
+        private Int16 año;
+        private String pais;
+        private string director;
 
         //Constructores
-      
-      public Pelicula()
-      {
-
-      }
+  
         //Métodos
-     
-        public void Imprime()
+
+     //get
+     public string GetTitulo(){return titulo;}
+     public Int16 GetAño(){return año;}
+     public string GetPais(){return pais;}
+     public string GetDirector(){return director;}
+     //set
+     public void SetTitulo(string titulo){this.titulo = titulo;}
+     public void SetAño(Int16 año){this.año = año;}
+     public void SetPais(string pais){this.pais = pais;}
+     public void SetDirector(string director){this.director = director;}
+    public void Imprime()
         {
-            Console.WriteLine($"{titulo} ({año})");
+           Console.WriteLine($"{titulo} ({año})");
 
         }
 
 
     }
-
+    
     public class Actor
     {
         //Propiedades
@@ -40,7 +46,7 @@ namespace Pelicula
         //Métodos 
         public void Imprime()
         {
-            //Console.WriteLine($"{Nombre} ({Año})");
+           // Console.WriteLine($"{Nombre} ({Año})");
         }
     }
 
@@ -54,13 +60,22 @@ namespace Pelicula
         static void Main(string[] args)
         {
             Pelicula p1 = new Pelicula();
-            p1.titulo = ("Promare");
-            p1.año = (2019);
-            p1.Imprime();
+            p1.SetTitulo("Promare");
+            p1.SetAño(2019);
+            //p1.set_pais("Japón");
+            //p1.set_director("Hiroyuki Imaishi");
+            //Console.WriteLine("{0}({1}){2}{3}", p1.GetTitulo(), p1.GetAño(), p1.SetPais(), p1.GetDirector());
+            Console.WriteLine("{0}({1})", p1.GetTitulo(), p1.GetAño());
+
             Pelicula p2 = new Pelicula();
-            p2.titulo = ("Method");
-            p2.año = (2017);
-            p2.Imprime();
+            p2.SetTitulo("Method");
+            p2.SetAño(2017);
+            //p2.set_pais("Corea del Sur");
+            //p2.set_director("Bang Eun-jin");
+            //Console.WriteLine("{0}({1}){2}{3}", p2.GetTitulo(), p2.GetAño(), p2.GetPais(), p2.GetDirector());
+            Console.WriteLine("{0}({1})", p2.GetTitulo(), p2.GetAño());
+
         }
     }
 }
+
